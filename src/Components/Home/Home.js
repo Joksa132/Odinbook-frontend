066d8c2cf134594d64af809f8f6bf170ee95103e
Joksa <nikolajoksimovic419@gmail.com>
2023-03-05 20:45:33 +0100

    implemented navbar and protectedroute

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,28 @@
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+import { useContext } from "react"
+import { UserContext } from "../../Context/UserContext"
+
+import Nav from "../Nav/Nav"
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+})
 
 function Home() {
+  //const { user } = useContext(UserContext)
 
+  return (
+    <>
+      <Nav />
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default Home
