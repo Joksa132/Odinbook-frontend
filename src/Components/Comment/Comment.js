eae667599ade8229587629e22dd36186a4d43ba2
Joksa <nikolajoksimovic419@gmail.com>
2023-03-18 23:10:30 +0100

    implemented comment delete

--- src/Components/Comment/Comment.js
+++ src/Components/Comment/Comment.js
@@ -1,12 +1,13 @@
 import { Typography, Button } from "@mui/material";
 import { Box } from "@mui/system";
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
 import axios from "axios";
 import { UserContext } from "../../Context/UserContext"
 import { useContext } from "react";
 
-function Comment({ comment, onLikedComment }) {
+function Comment({ comment, onLikedComment, comments, setComments }) {
   const { user } = useContext(UserContext)
 
   const likeComment = () => {
@@ -17,16 +18,29 @@ function Comment({ comment, onLikedComment }) {
       .then((res) => onLikedComment())
   }
 
+  const handleDelete = () => {
+    const confirmation = window.confirm("Are you sure you want to delete this comment?")
+    confirmation &&
+      axios.delete(`http://localhost:4000/comment/delete/${comment._id}`, { headers: { Authorization: `Bearer ${localStorage.getItem("token")}` } })
+        .then((res) => {
+          setComments(comments.filter((comment) => comment._id !== res.data._id))
+        })
+        .catch((err) => console.log(err))
+  }
+
   return (
     <Box
       sx={{
-        backgroundColor: "rgb(36,37,38)",
-        my: "5px"
+        backgroundColor: "rgb(36,37,38)"
       }}
     >
-      <Typography variant="subtitle2">
-        {`${comment.createdBy.firstName} ${comment.createdBy.lastName}`}
-      </Typography>
+      <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Typography variant="subtitle2" sx={{ marginTop: "10px" }}>
+          {`${comment.createdBy.firstName} ${comment.createdBy.lastName}`}
+        </Typography>
+        <Button startIcon={<DeleteForeverIcon fontSize="small" />} onClick={handleDelete}>Delete</Button>
+      </Box>
+
       <Typography variant="subtitle1" color="#e0e0e0">
         {new Date(comment.createdAt).toLocaleDateString('en-GB')}
       </Typography>
