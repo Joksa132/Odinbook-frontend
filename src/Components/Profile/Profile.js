d965b4620986a5b027c363f37bfa98e66e898d4e
Joksa <nikolajoksimovic419@gmail.com>
2023-03-21 13:38:23 +0100

    implemented edit, delete and submit post

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -7,6 +7,7 @@ import { Box } from '@mui/system';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Post from '../Post/Post';
+import PostForm from '../PostForm/PostForm';
 
 const darkTheme = createTheme({
   palette: {
@@ -20,6 +21,7 @@ function Profile() {
   const [posts, setPosts] = useState([])
   const [loading, setLoading] = useState(true);
   const [postsLoading, setPostsLoading] = useState(true)
+  const [edit, setEdit] = useState(null)
 
   useEffect(() => {
     async function getProfileInfo() {
@@ -53,6 +55,51 @@ function Profile() {
     getProfilePosts()
   }, [id])
 
+  const handleLikePost = () => {
+    axios.get(`http://localhost:4000/post/${id}`)
+      .then((res) => setPosts(res.data))
+      .catch((err) => console.log(err))
+  }
+
+  const handleDelete = (id) => {
+    const confirmation = window.confirm("Are you sure you want to delete this post?")
+    confirmation &&
+      axios.delete(`http://localhost:4000/post/delete/${id}`, { headers: { Authorization: `Bearer ${localStorage.getItem("token")}` } })
+        .then((res) => {
+          setPosts(posts.filter((post) => post._id !== res.data._id))
+        })
+        .catch((err) => console.log(err))
+  }
+
+  const onSubmit = (description) => {
+    const newPost = {
+      description
+    }
+    axios.post("http://localhost:4000/post/new", newPost, { headers: { Authorization: `Bearer ${localStorage.getItem("token")}` } })
+      .then((res) => { setPosts(prevPosts => [res.data, ...prevPosts]) })
+      .catch((err) => console.log(err))
+  }
+
+  const handleEdit = (description, postId) => {
+    setEdit({
+      description,
+      postId
+    })
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
+  }
+
+  const onEditSubmit = (description, id) => {
+    const updatePost = {
+      description
+    }
+    axios.put(`http://localhost:4000/post/update/${id}`, updatePost)
+      .then(res => {
+        setPosts(posts.map(post => (post._id === res.data._id ? res.data : post)))
+        setEdit(null)
+      })
+      .catch(err => console.log(err))
+  }
+
   return (
     <>
       <Nav />
@@ -60,10 +107,19 @@ function Profile() {
         <CssBaseline />
         <Container>
           {!loading ?
-            <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", marginTop: "20px" }}>
-              <Typography variant='h4'>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                marginTop: "20px"
+              }}
+            >
+              <Typography variant='h4' sx={{ marginBottom: "10px" }}>
                 {profileInfo[0].firstName + " " + profileInfo[0].lastName}
               </Typography>
+              <PostForm onSubmit={onSubmit} editValue={edit?.description} id={edit?.postId} onEdit={onEditSubmit} />
               <Box
                 sx={{
                   display: "flex",
@@ -76,7 +132,13 @@ function Profile() {
                 {!postsLoading ?
                   posts.map(post => {
                     return (
-                      <Post key={post._id} post={post} />
+                      <Post
+                        key={post._id}
+                        post={post}
+                        onLikedPost={handleLikePost}
+                        handleDelete={() => handleDelete(post._id)}
+                        handleEdit={() => handleEdit(post.description, post._id)}
+                      />
                     )
                   }) : <Box>Loading</Box>}
               </Box>
